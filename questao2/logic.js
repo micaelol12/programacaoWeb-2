16053e2d4e006e325087e6e4ce03060724b7eed1
Micael Luan Conti <mlconti@furb.br>
2023-04-18 21:11:14 -0300

    finishing seconds question

--- questao2/logic.js
+++ questao2/logic.js
@@ -1,11 +1,16 @@
 const TEXT_INPUT_ID = "text_input";
 const TEXT_SHOW_ID = "text_show";
+const TEXT_COUNT = "num_caracteres"
 
 const textInput = document.getElementById(TEXT_INPUT_ID);
 const textShow = document.getElementById(TEXT_SHOW_ID);
-
+const textCount = document.getElementById(TEXT_COUNT);
 
 
 function textChange() {
 textShow.innerHTML = textInput.value
+}
+
+function ContarCaracteres() {
+    textCount.innerHTML = 'O tamanho atual do campo é de: ' + textInput.value.length
 }
